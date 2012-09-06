fe7fa54edef01bcdff7e88449ad6053380c51e5b
Subhash Pant <subhash.pant@gmail.com>
2012-09-05 23:49:52 -0500

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -8,15 +8,22 @@ public class Operations {
 	
 	DBInteraction dbInter = new DBInteraction();
 	
-	public boolean CreateProduct(int CategoryID, String productDescription, boolean IsTaxable, int priceSchemeID)
+	/**
+	 * Adds a product into the products table.
+	 * Required fields are: CategoryID, PriceSchemeID, ProductName(description),
+	 * and whether or not the product is taxable.
+	 * @return 
+	 * @throws SQLException 
+	*/
+	public boolean CreateProduct(int CategoryID, int PricingSchemeID, String productDescription, boolean IsTaxable)
 	{
 		Connection conn = dbInter.getConnection();
 		try {
 			CallableStatement cs = conn.prepareCall("{CALL CreateProduct(?, ?, ?, ?)}");
 			cs.setInt(1, CategoryID);
-			cs.setString(2, productDescription);
-			cs.setBoolean(3, IsTaxable);
-			cs.setInt(4, priceSchemeID);
+			cs.setInt(2, PricingSchemeID);
+			cs.setString(3, productDescription);
+			cs.setBoolean(4, IsTaxable);
 			cs.executeQuery();	
 
 		} 
@@ -30,10 +37,36 @@ public class Operations {
 		}	
 		return true;
 	}
-	
+	/**
+	 * Adds a new pricing scheme
+	 * @return 
+	 * @throws SQLException 
+	*/
+	public boolean CreatePriceScheme(int PriceSchemePricesID, int Quantity, float Price, String PricingSchemeDesc)
+	{
+		Connection conn = dbInter.getConnection();
+		try {
+			CallableStatement cs = conn.prepareCall("{CALL CreatePriceScheme(?, ?, ?, ?)}");
+			cs.setInt(1, PriceSchemePricesID);
+			cs.setInt(2, Quantity);
+			cs.setFloat(3, Price);
+			cs.setString(4, PricingSchemeDesc);
+			cs.executeQuery();	
+			//System.out.println("New Pricing Scheme created.");
+		} 
+		catch(NullPointerException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		catch (SQLException e) {
+			System.out.println("Error occurred while creating pricing Scheme. " + e.getMessage());
+		}	
+		return true;
+	}
 	
 	/**
-	 * 
+	 * Removed a product by ProductID
 	 * @return 
 	 * @throws SQLException 
 	*/
@@ -57,12 +90,15 @@ public class Operations {
 		}	
 		
 	}
-	
+	/**
+	 * Returns the product with Name and Category name
+	 * @param productID
+	 */
 	public void getProducts(int productID)
 	{
 		
 		
-		Connection conn = dbInter.getConnection();
+		Connection conn = ((DBInteraction) dbInter).getConnection();
             
 		
 		try {
@@ -82,8 +118,68 @@ public class Operations {
 
            
        }
+	/**
+	 * Returns the product and all the pricing scheme attached to the product
+	 * @param productID
+	 */
+	public void getProductPricingScheme(int productID)
+	{
+		
+		
+		Connection conn = ((DBInteraction) dbInter).getConnection();
+            
+		
+		try {
+			CallableStatement cStmt = conn.prepareCall("{CALL LoadPriceScheme(?)}");
+			 cStmt.setInt(1, productID);
+	         cStmt.execute();
+	           ResultSet rs1 = cStmt.getResultSet();
+	           while (rs1.next()) {
+	                System.out.println("Product Name: " + rs1.getString("ProductDesc") + " " + ", Number of Items: " + rs1.getInt("Quantity")
+	                		+ ", Price: " + rs1.getInt("Price") + ", Price Scheme description: " + rs1.getString("PricingSchemeDesc") );
+	           }
+	           rs1.close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+          
+
+           
+   }
+	
+	/**
+	 * Returns the product and all the pricing scheme attached to the product
+	 * @param productID
+	 */
+	public void getCategory()
+	{
+		
+		
+		Connection conn = ((DBInteraction) dbInter).getConnection();
+            
+		
+		try {
+			CallableStatement cStmt = conn.prepareCall("{CALL LoadCategory}");
+			 
+	         cStmt.execute();
+	           ResultSet rs1 = cStmt.getResultSet();
+	           while (rs1.next()) {
+	                System.out.println("CategoryID: " + rs1.getInt("CategoryID") + ", Category Description " + rs1.getString("CategoryDesc") );
+	           }
+	           rs1.close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+          
+
+           
+   }
+	
+	
+	
 	
 	
 	 
 }
-
